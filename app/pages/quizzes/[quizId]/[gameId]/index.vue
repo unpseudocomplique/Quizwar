0670dd055f593db29c01850e6c24fad9dc335ecb
Mathieu <mathieu@bestconsultant.fr>
2024-08-25 22:25:35 +0200

    add particles

--- app/pages/quizzes/[quizId]/[gameId]/index.vue
+++ app/pages/quizzes/[quizId]/[gameId]/index.vue
@@ -141,6 +141,7 @@ const shareGame = async () => {
         <UDashboardPanel grow>
             <UDashboardNavbar title="Home">
             </UDashboardNavbar>
+
             <UDashboardPanelContent v-if="!isGameStarted" class="flex flex-col items-center justify-center gap-4">
 
                 <p class="cursor-pointer text-xl" @click="copy(game.display)">Game code : {{ game.display }} <icon
@@ -159,6 +160,60 @@ const shareGame = async () => {
 
             </UDashboardPanelContent>
             <UDashboardPanelContent v-else-if="!isGameOver">
+                <client-only>
+                    <vue-particles id="tsparticles" :options="{
+                        fpsLimit: 120,
+                        interactivity: {
+                            events: {
+                                onHover: {
+                                    enable: true,
+                                    mode: 'repulse'
+                                },
+                            },
+                            modes: {
+                                bubble: {
+                                    distance: 400,
+                                    duration: 2,
+                                    opacity: 0.8,
+                                    size: 40
+                                },
+                                push: {
+                                    quantity: 4
+                                },
+                                repulse: {
+                                    distance: 200,
+                                    duration: 0.4
+                                }
+                            }
+                        },
+                        particles: {
+                            move: {
+                                direction: 'none',
+                                enable: true,
+                                outModes: 'bounce',
+                                random: false,
+                                speed: 3,
+                                straight: false
+                            },
+                            number: {
+                                density: {
+                                    enable: true,
+                                },
+                                value: 80
+                            },
+                            opacity: {
+                                value: 0.1
+                            },
+                            shape: {
+                                type: 'circle'
+                            },
+                            size: {
+                                value: { min: 1, max: 5 }
+                            }
+                        },
+                        detectRetina: true
+                    }" />
+                </client-only>
 
                 <u-card class="h-full flex flex-col" v-if="!showScore"
                     :ui="{ body: { base: 'h-full flex' }, padding: 'px-4 py-5 sm:p-6' }">
