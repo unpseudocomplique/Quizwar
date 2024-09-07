0d82a7e84e6cbdeeee26745102b0f0a45b30c17f
Mathieu <mathieu@bestconsultant.fr>
2024-09-07 22:28:22 +0200

    update admin

---
+++ app/components/admin/quiz/list/index.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { computed, ref } from 'vue'
+const { data: quizzes } = await useFetch('/api/quizzes')
+
+</script>
+
+<template>
+    <div class="flex flex-col gap-4 w-full">
+        <ul class="grow">
+            <li class="grow" v-for="quiz in quizzes">
+                <router-link :to="`/admin/quizzes/${quiz.id}`">
+                    <u-card>
+                        <p>{{ quiz.display }}</p>
+                    </u-card>
+                </router-link>
+            </li>
+        </ul>
+    </div>
+</template>
--- app/pages/admin/quizzes/index.vue
+++ app/pages/admin/quizzes/index.vue
@@ -1,16 +1,80 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+const isLoadingQuiz = ref(false)
+const quizTheme = ref('')
 
-const { data: quizzes } = await useFetch('/api/quizzes')
+const items = [
+    {
+        slot: 'create',
+        label: 'Create quiz'
+    },
+    {
+        slot: 'quizzes',
+        label: 'Quizzes'
+    },
+    {
+        slot: 'seed',
+        label: 'Seed db'
+    },
+]
+
+
+const models = ['openAi', 'vertexAI']
+const modelSelected = ref(models[0])
+
+
+const seedDB = async () => {
+    try {
+        await useFetch('/api/seed', { method: 'POST' })
+        alert('Seeded')
+    } catch (e) {
+        alert('alert error')
+    }
+
+}
+
+
+const createQuiz = async () => {
+    try {
+
+        isLoadingQuiz.value = true
+        const response = await $fetch('/api/quiz/create', {
+            method: 'POST',
+            query: { topic: quizTheme.value, modelType: modelSelected.value }
+        })
+        alert('Quiz created')
+    } catch (e) {
+        console.log(e)
+        alert('error')
+    }
+    isLoadingQuiz.value = false
+}
 
 </script>
 
 <template>
-    <div class="p-4">
-        <h1>Quizzes</h1>
-        <ul>
-            <li v-for="quiz in quizzes"><router-link :to="`/admin/quizzes/${quiz.id}`">{{ quiz.display }}</router-link>
-            </li>
-        </ul>
+    <div class="p-4 grow">
+        <UTabs :items="items" class="w-full">
+            <template #quizzes="{ item }">
+                <admin-quiz-list />
+            </template>
+            <template #seed="{ item }">
+
+                <u-button @click="seedDB" size="xl">
+                    Seed db
+                </u-button>
+            </template>
+            <template #create="{ item }">
+                <div class="flex gap-4 flex-wrap">
+
+                    <USelect v-model="modelSelected" :options="models" />
+                    <u-input v-model="quizTheme" placeholder="Topic" size="xl" />
+                    <u-button @click="createQuiz" size="xl" :disabled="isLoadingQuiz">
+                        Create quiz
+                    </u-button>
+                </div>
+            </template>
+        </UTabs>
+
+
     </div>
 </template>
