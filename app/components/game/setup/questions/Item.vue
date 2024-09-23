bcf8e381dad212de629989fdcde343776d803d68
Mathieu <mathieu@bestconsultant.fr>
2024-09-23 11:13:22 +0200

    bump version

--- app/components/game/setup/questions/Item.vue
+++ app/components/game/setup/questions/Item.vue
@@ -1,37 +1,34 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, ref,useTemplateRef } from 'vue'
 
 import { useFloating, autoPlacement, flip, shift, hide } from '@floating-ui/vue';
 const question = defineModel()
-const reference = ref(null);
-const floating = ref(null);
+
+const reference = useTemplateRef('reference')
+const floating = useTemplateRef('floating')
+
 
 const isHidden = ref(true)
 
 const { floatingStyles, update, placement } = useFloating(reference, floating, {
     middleware: [
         autoPlacement({
-            // padding: 10, // Adds padding around the boundaries to avoid clipping
-            boundary: 'clippingAncestors',
-            alignment: 'start',
 
-
+            elementContext: 'floating'
         }),
 
         shift({
-            mainAxis: true, // Shifts along the main axis to avoid overflow
-            crossAxis: true,
-            rootBoundary: 'viewport', // Ensure shifts are constrained within the viewport
+
         })
 
     ],
 });
 
-function hideItem() {
+const hideItem = () => {
     isHidden.value = true;
 }
 
-function show() {
+const show = () => {
     isHidden.value = false;
 }
 
@@ -42,28 +39,33 @@ function show() {
         class="w-32 rounded-lg h-6 bg-gray-200 dark:bg-gray-700 cursor-pointer relative">
 
 
-    </div>
-    <u-card class="z-10 w-96" ref="floating" :style="{
-        ...floatingStyles,
-        display: isHidden ? 'none' : 'block',
-    }">
-        <div class="flex flex-col gap-2">
-            <!-- <u-card>
+        <u-card class="z-10 w-96" ref="floating" :style="{
+            ...floatingStyles,
+            transform: `${floatingStyles.transform} translateX(-100%)`,
+            display: isHidden ? 'none' : 'block',
+        }">
+            <div class="flex flex-col gap-2">
+                <!-- <u-card>
                     <template #header>
                         <UBlogPost title="x5" description="Les questions rapportent 5x plus de points"
                             class="cursor-pointer" />
                     </template>
 </u-card> -->
-            <u-card>
-                <!-- <template #header>
+                <u-card>
+                    <!-- <template #header>
                         <UBlogPost title="Fastpass"
                             description="3 points pour le premier, 1 pour le deuxième, 1 pour le 3ème et -1 pour les suivants."
                             class="cursor-pointer" />
                     </template> -->
-                <pre>
-        {{ floatingStyles }}
-    </pre>
-            </u-card>
-        </div>
-    </u-card>
+                    <pre>
+                    {{ floatingStyles }}
+        
+                    </pre>
+                    <pre>
+                        {{  placement  }}
+                    </pre>
+                </u-card>
+            </div>
+        </u-card>
+    </div>
 </template>
