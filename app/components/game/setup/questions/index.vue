ca8adac8979097f2dd217ef3138c275ee301bce2
Mathieu <mathieu@bestconsultant.fr>
2024-10-19 16:58:49 +0200

    remove debug

--- app/components/game/setup/questions/index.vue
+++ app/components/game/setup/questions/index.vue
@@ -15,30 +15,5 @@ const gameStore = useGameStore()
             <game-setup-questions-item v-for="(question, index) in game.quiz.questions" v-model:game="game"
                 :index="index" :key="question.id" v-model="game.quiz.questions[index]" />
         </div>
-
-        <table>
-            <caption>
-                Front-end web developer course 2021
-            </caption>
-            <thead>
-                <tr>
-                    <th scope="col">id</th>
-                    <th scope="col">gameId</th>
-                    <th scope="col">parameter</th>
-                    <th scope="col">rangeStart</th>
-                    <th scope="col">rangeEnd</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr v-for="parameter in gameStore.game.parameters" :key="parameter.id">
-                    <th class="border border-gray-200 dark:border-gray-700">{{ parameter.id }}</th>
-                    <td class="border border-gray-200 dark:border-gray-700">{{ parameter.gameId }}</td>
-                    <td class="border border-gray-200 dark:border-gray-700">{{ parameter.parameter }}</td>
-                    <td class="border border-gray-200 dark:border-gray-700">{{ parameter.rangeStart }}</td>
-                    <td class="border border-gray-200 dark:border-gray-700">{{ parameter.rangeEnd }}</td>
-                </tr>
-            </tbody>
-        </table>
-
     </div>
 </template>
